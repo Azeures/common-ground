3ea30ac258da15bee55beb71c93e7c415f81ccda
Azeures <joeljonast@gmail.com>
2017-03-19 01:56:59 -0400

    pricing

--- client/main.js
+++ client/main.js
@@ -5,7 +5,7 @@ import './main.html';
 
 $(document).scroll(function() {
   var y = $(this).scrollTop();
-  if (y > 200) {
+  if (y > 150) {
     $('.nav-fixed').fadeIn('slow');
     $('.nav-fixed').css('display', 'block');
   } else {
@@ -14,11 +14,15 @@ $(document).scroll(function() {
   }
 });
 
+$('.price-selector').hover(function() {
+  $('.price-selector').css('display', 'none');
+});
+
 // Template.main.onCreated(function siteOnCreated() {
 // });
-
+//
 // Template.main.helpers({
 // });
-
+//
 // Template.main.events({
 // });
