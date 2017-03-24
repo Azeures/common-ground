5a3bc32f46951e5eddc50847fae151a1b824173e
Azeures <joeljonast@gmail.com>
2017-03-24 01:19:17 -0400

    brb dead

--- client/landing.js
+++ client/landing.js
@@ -40,12 +40,12 @@ $('.price-selector').hover(function() {
 
 $(document).on({
     mouseenter: function() {
-        $('.price-starter').css('transform', 'scale(1.08, 1.08)');
+        // $('.price-starter').css('transform', 'scale(1.08, 1.08)');
         $('.price-starter > article').css('box-shadow', '0 0 1em gold');
         $('.starter-btn').css('opacity', '100');
     },
     mouseleave: function() {
-        $('.price-starter').css('transform', 'scale(1, 1)');
+        // $('.price-starter').css('transform', 'scale(1, 1)');
         $('.starter-btn').css('opacity', '0');
         $('.price-starter > article').css('box-shadow', 'none');
     },
@@ -108,12 +108,12 @@ $(document).on({
 
 $(document).on({
     mouseenter: function() {
-        $('.price-basic').css('transform', 'scale(1.08, 1.08)');
+        // $('.price-basic').css('transform', 'scale(1.08, 1.08)');
         $('.basic-btn').css('opacity', '100');
         $('.price-basic > article').css('box-shadow', '0 0 1em gold');
     },
     mouseleave: function() {
-        $('.price-basic').css('transform', 'scale(1, 1)');
+        // $('.price-basic').css('transform', 'scale(1, 1)');
         $('.basic-btn').css('opacity', '0');
         $('.price-basic > article').css('box-shadow', 'none');
     },
@@ -183,12 +183,12 @@ $(document).on({
 
 $(document).on({
     mouseenter: function() {
-        $('.price-pro').css('transform', 'scale(1.08, 1.08)');
+        // $('.price-pro').css('transform', 'scale(1.08, 1.08)');
         $('.pro-btn').css('opacity', '100');
         $('.price-pro > article').css('box-shadow', '0 0 1em gold');
     },
     mouseleave: function() {
-        $('.price-pro').css('transform', 'scale(1, 1)');
+        // $('.price-pro').css('transform', 'scale(1, 1)');
         $('.pro-btn').css('opacity', '0');
         $('.price-pro > article').css('box-shadow', 'none');
     },
